82b6e1a4bd8ecb1df493625698bde1a205a7e306
Loc Hoang <l_hoang@utexas.edu>
2017-03-23 22:39:23 -0500

    programs directoy, tester file

---
+++ src/programs/Tester.scala
@@ -0,0 +1,15 @@
+package programs
+
+
+import scala-chef-redux._
+import scala.language.postfixOps
+
+object Tester extends ScalaChefRedux {
+  def main(args: Array[String]) = {
+    Title - "hello thar";
+
+    Ingredients ;
+
+    Method ;
+  }
+}
