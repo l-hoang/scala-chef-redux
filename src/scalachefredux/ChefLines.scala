f129f7758157816d2496e21ee181251c7209b075
Loc Hoang <l_hoang@utexas.edu>
2017-03-25 11:41:55 -0500

    Added lines necessary for hello world

--- src/scalachefredux/ChefLines.scala
+++ src/scalachefredux/ChefLines.scala
@@ -4,7 +4,18 @@ package scalachefredux
 
 abstract sealed class ChefLine
 
-case class PutLine() extends ChefLine
+case class Read(ingredient: String) extends ChefLine // Take
+case class Push(bowlNumber: Int) extends ChefLine // Put
+case class Pop(bowlNumber: Int) extends ChefLine // Fold
+
+case class Liquify(ingredient: String) extends ChefLine // Liquify
+case class LiquifyBowl(bowlNumber: Int) extends ChefLine // Liquify
+
+case class CopyStack(bowlNumber: Int, dishNumber: Int) extends ChefLine // Pour
+
+case object Break extends ChefLine // Set
+
+case class PrintStacks(num: Int) extends ChefLine // Serves
 
 // TODO
 
