b9757c8f48c2e7b3d1ff95fa0d4d0019190dd8d0
Loc Hoang <l_hoang@utexas.edu>
2017-03-31 19:48:30 -0500

    now holds ChefIngredient, comment location changes

--- src/scalachefredux/LineBuilder.scala
+++ src/scalachefredux/LineBuilder.scala
@@ -8,7 +8,6 @@ class LineBuilder {
   case object M_TITLE extends ParseMode
   case object M_INGREDIENT extends ParseMode
   case object M_METHOD extends ParseMode
-
   case object M_LIMBO extends ParseMode
 
 
@@ -26,52 +25,56 @@ class LineBuilder {
 
   // The current recipe being parsed by the line builder
   var currentRecipe = ""
-
+  
+  case object NULLINGREDIENT extends ChefIngredient("null", I_NULL)
+  
   // Various things that need to be used to build lines
   var heldString = ""
   var heldNumber = -1
   var stackNumber1 = -1 
   var stackNumber2 = -1
+  var heldIngredient: ChefIngredient = NULLINGREDIENT
+
 
   ////////////////
   // Assertions //
   ////////////////
   /* Assert certain things about the current line builder state */
 
-  def assertTitle = {
   /* Title assertion */
+  def assertTitle = {
     mode match {
       case M_TITLE =>
       case _ => throw new RuntimeException("Assert title mode failed")
     }
   }
 
-  def assertIngredient = {
   /* Ingredient assertion */
+  def assertIngredient = {
     mode match {
       case M_INGREDIENT =>
       case _ => throw new RuntimeException("Assert ingredient mode failed")
     }
   }
 
-  def assertMethod = {
   /* Method assertion */
+  def assertMethod = {
     mode match {
       case M_METHOD =>
       case _ => throw new RuntimeException("Assert method mode failed")
     }
   }
 
-  def assertLimbo = {
   /* Limbo assertion */
+  def assertLimbo = {
     mode match {
       case M_LIMBO =>
       case _ => throw new RuntimeException("Assert limbo mode failed")
     }
   }
 
-  def assertNoOp = {
   /* Assert there is currently no op set in the line builder */
+  def assertNoOp = {
     currentOp match {
       case E_NONE =>
       case _ => throw new RuntimeException("Assert no op failed")
@@ -83,70 +86,64 @@ class LineBuilder {
   ////////////////////
   /* Methods to alter the state of the line builder */
 
-  def modeTitle = {
   /* Switch to title mode */
+  def modeTitle = {
     assertNoOp
     clearData
     mode = M_TITLE
   }
 
-  def modeIngredient = {
   /* Switch to ingredient mode */
+  def modeIngredient = {
     assertLimbo
     assertNoOp
     clearData
     mode = M_INGREDIENT
   }
 
-  def modeMethod = {
   /* Switch to method mode */
+  def modeMethod = {
     assertNoOp
     clearData
     mode = M_METHOD
   }
 
-  def modeLimbo = {
   /* Switch to limbo mode (i.e. needs to switch modes) */
+  def modeLimbo = {
     assertNoOp
     clearData
     mode = M_LIMBO
   }
 
-  def setCurrentRecipe(recipe: String) = {
   /* Set the current recipe being parsed by the program */
-    currentRecipe = recipe
-  }
+  def setCurrentRecipe(recipe: String) = currentRecipe = recipe
 
-  def setOp(newOp: ChefOp) = {
   /* Change the current operation that the line builder is building */
+  def setOp(newOp: ChefOp) = {
     // TODO make it so certain ops can't be changed (i.e. most of them)
     currentOp = newOp
   }
 
-  def setString(newString: String) = {
   /* Set the string the line builder is holding */
-    heldString = newString
-  }
-
-  def setNumber(newNumber: Int) = {
+  def setString(newString: String) = heldString = newString
   /* Set the number the line builder is holding */
-    heldNumber = newNumber
-  }
-
-  def setStackNumber1(newNumber: Int) = {
+  def setNumber(newNumber: Int) = heldNumber = newNumber 
   /* Set the first stack number. */
-    stackNumber1 = newNumber
-  }
-
-  def setStackNumber2(newNumber: Int) = {
+  def setStackNumber1(newNumber: Int) = stackNumber1 = newNumber
   /* Set the second stack number. */
-    stackNumber2 = newNumber
+  def setStackNumber2(newNumber: Int) = stackNumber2 = newNumber
+
+  /* Set the held ingredient. */
+  def setIngredient(newIngredient: ChefIngredient) = {
+    assertIngredient
+    heldIngredient = newIngredient
   }
 
-  def clearData = {
   /* Clear all currently held line state (except the title) by resetting it 
    * to some default value. */
+  def clearData = {
     currentOp = E_NONE
+    heldIngredient = NULLINGREDIENT
     heldString = ""
     heldNumber = -1
     stackNumber1 = -1
@@ -158,8 +155,8 @@ class LineBuilder {
   //////////////////////
   /* Methods used to get things from the line builder */
 
-  def finishLine = {
   /* Return a line with the appropriate information filled in. */
+  def finishLine = {
     // TODO
 
     // clear the data in preparation for the next line
