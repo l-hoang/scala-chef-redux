73abd1af19ea70b01cd4e59622e60c264a96c3fa
Loc Hoang <l_hoang@utexas.edu>
2017-04-04 10:17:18 -0500

    added lines to push hello world twice

--- src/programs/Tester.scala
+++ src/programs/Tester.scala
@@ -35,16 +35,53 @@ object Tester extends ScalaChefRedux {
     Ingredients;
 
     1 ct "tofu";
-    72 g "haricot_beans" ;
+    72 g "haricot beans" ;
     101 ct "eggs";
     108 g "lard";
     111 cups "oil";
     32 ct "zucchinis";
     119 ml "water";
-    114 g "red_salmon";
-    100 g "dijon_mustard";
+    114 g "red salmon";
+    100 g "dijon mustard";
     33 ct "potatoes";
 
     Method;
+
+    Put the "potatoes" into mixing bowl 1;
+    Put the "dijon mustard" into mixing bowl 1;
+    Put some "lard" into mixing bowl 1;
+    Put the "red salmon" into mixing bowl 1;
+    Put some "oil" into mixing bowl 1;
+    Put some "water" into mixing bowl 1;
+    Put the "zucchinis" into mixing bowl 1;
+    Put some "oil" into mixing bowl 1;
+    Put some "lard" into mixing bowl 1;
+    Put some "lard" into mixing bowl 1;
+    Put the "eggs" into mixing bowl 1;
+
+    Put the "haricot beans" into mixing bowl;
+    Put the "potatoes" into the mixing bowl;
+    Put the "dijon mustard" into the mixing bowl;
+    Put some "lard" into the mixing bowl;
+    Put the "red salmon" into the mixing bowl;
+    Put some "oil" into the mixing bowl;
+    Put some "water" into the mixing bowl;
+    Put the "zucchinis" into the mixing bowl;
+    Put some "oil" into the mixing bowl;
+    Put some "lard" into the mixing bowl;
+    Put some "lard" into the mixing bowl;
+    Put the "eggs" into the mixing bowl;
+    Put the "haricot beans" into the mixing bowl;
+
+    //take the "thing" from the refridge
+    //take "thing" from fridge
+    //fold the <thing> into bowl <num>
+    //add the <thing> into bowl <n>
+    //remove the <thing> from bowl <>
+    //divide the <hing from bowl <>
+    //add dry ingredients to bowl <>
+    //liquify <thing>
+    //liquify the <thing>
   }
+
 }
