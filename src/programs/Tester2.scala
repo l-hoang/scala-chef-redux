4f04fb4fe514b7a97855b4f50769d60b05f645b4
Loc Hoang <l_hoang@utexas.edu>
2017-04-05 12:58:37 -0500

    tester with 2 recipes

---
+++ src/programs/Tester2.scala
@@ -0,0 +1,93 @@
+package programs
+
+import scalachefredux._
+import scala.language.postfixOps
+
+object Tester2 extends ScalaChefRedux {
+  def main(args: Array[String]) = {
+    Title - "Recipe 1";
+
+    // add some ingredients
+    Ingredients;
+
+    1 ct "tofu";
+    72 g "haricot beans" ;
+    101 ct "eggs";
+    108 g "lard";
+    111 cups "oil";
+    32 ct "zucchinis";
+    119 ml "water";
+    114 g "red salmon";
+    100 g "dijon mustard";
+    33 ct "potatoes";
+
+    Method;
+
+    Put the "potatoes" into mixing bowl 1;
+    Put the "dijon mustard" into mixing bowl 1;
+    Put some "lard" into mixing bowl 1;
+    Put the "red salmon" into mixing bowl 1;
+    Put some "oil" into mixing bowl 1;
+    Put some "water" into mixing bowl 1;
+    Put the "zucchinis" into mixing bowl 1;
+    Put some "oil" into mixing bowl 1;
+    Put some "lard" into mixing bowl 1;
+    Put some "lard" into mixing bowl 1;
+    Put the "eggs" into mixing bowl 1;
+
+    Put the "haricot beans" into mixing bowl;
+    Put the "potatoes" into the mixing bowl;
+    Put the "dijon mustard" into the mixing bowl;
+    Put some "lard" into the mixing bowl;
+    Put the "red salmon" into the mixing bowl;
+    Put some "oil" into the mixing bowl;
+    Put some "water" into the mixing bowl;
+    Put the "zucchinis" into the mixing bowl;
+    Put some "oil" into the mixing bowl;
+    Put some "lard" into the mixing bowl;
+    Put some "lard" into the mixing bowl;
+    Put the "eggs" into the mixing bowl;
+    Put the "haricot beans" into the mixing bowl;
+
+    Liquefy the contents of the mixing bowl;
+    Liquefy the contents of mixing bowl 1;
+
+    Pour the contents of mixing bowl 1 into baking dish 2;
+    Pour the contents of mixing bowl 1 into the baking dish; 
+    Pour the contents of the mixing bowl into the baking dish; 
+    Pour the contents of the mixing bowl into baking dish 2; 
+
+    //take the "thing" from the refridge
+    //take "thing" from fridge
+    //fold the <thing> into bowl <num>
+    //add the <thing> into bowl <n>
+    //remove the <thing> from bowl <>
+    //divide the <hing from bowl <>
+    //add dry ingredients to bowl <>
+    //liquify <thing>
+    //liquify the <thing>
+    //printLines;
+
+    Recipe serves 1;
+
+    Title - "Recipe 2";
+
+    Ingredients;
+
+    1 ct "tofu";
+    72 g "haricot beans" ;
+    101 ct "eggs";
+    108 g "lard";
+    111 cups "oil";
+    32 ct "zucchinis";
+    119 ml "water";
+    114 g "red salmon";
+    100 g "dijon mustard";
+    33 ct "potatoes";
+
+    Method;
+
+    Enjoy your meal;
+  }
+
+}
